759108f7a51689fa257c1f83dd4155d021afbec4
Brian Zoetewey <brian.zoetewey@ccci.org>
2016-01-15 16:59:11 -0500

    Try running two instances of the app, one for proxy_callback, other for everything else.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM cruglobal/base-image-ruby:latest
 MAINTAINER cru.org <wmd@cru.org>
 
-COPY puma.rb /etc/puma.rb
+COPY docker/nginx-httpd.conf /usr/local/openresty/nginx/conf.d/webapp.conf
+COPY docker/puma_app.rb /etc/puma_app.rb
+COPY docker/puma_callback.rb /etc/puma_callback.rb
+COPY docker/supervisord-*.conf /etc/supervisor/conf.d/
--- docker/puma_callback.rb
+++ docker/puma_callback.rb
@@ -1,10 +1,7 @@
-workers 4
-queue_requests false
+workers 2
 shared_dir = '/opt/puma'
 rails_env = ENV['ENVIRONMENT'] || "production"
 environment rails_env
-bind "unix://#{shared_dir}/sockets/puma.sock"
+bind "unix://#{shared_dir}/sockets/puma.callback.sock"
 
 stdout_redirect "/var/log/puma/stdout.log", "/var/log/puma/stderr.log", true
-
-preload_app!
