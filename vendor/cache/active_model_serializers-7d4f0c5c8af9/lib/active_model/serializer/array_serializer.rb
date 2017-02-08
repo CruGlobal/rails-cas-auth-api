7ca96035a11c0c7957218a3c77e1a9f3cacab714
Brian Zoetewey <brian.zoetewey@ccci.org>
2017-02-08 13:04:24 -0500

    Removed source gems from cache

--- vendor/cache/active_model_serializers-7d4f0c5c8af9/lib/active_model/serializer/array_serializer.rb
+++
@@ -1,9 +0,0 @@
-require 'active_model/serializer/collection_serializer'
-class ActiveModel::Serializer
-  class ArraySerializer < CollectionSerializer
-    def initialize(*)
-      warn "Calling deprecated ArraySerializer in #{caller[0..2].join(', ')}. Please use CollectionSerializer"
-      super
-    end
-  end
-end
