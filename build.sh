3a3bd2de06e91017cac1d91be5cca514cb109adb
Brian Zoetewey <brian.zoetewey@ccci.org>
2017-06-06 14:59:49 -0400

    Update docker build.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build \
+    -t 056154071827.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME:$GIT_COMMIT-$BUILD_NUMBER .
+rc=$?
+
+if [ $rc -ne 0 ]; then
+  echo -e "Docker build failed"
+  exit $rc
+fi
