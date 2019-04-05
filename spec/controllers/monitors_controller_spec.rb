2d9ad71d995e03650e2d573a6cbea120b64b18d2
Justin Sabelko <justin.sabelko@cru.org>
2019-04-05 13:37:43 -0500

    Upgrade to Rails 5.2.3

--- spec/controllers/monitors_controller_spec.rb
+++ spec/controllers/monitors_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe MonitorsController, type: :controller do
   describe 'GET #lb' do
     it 'responds successfully with an HTTP 200 OK' do
       get :lb
-      expect(response).to be_success
+      expect(response).to be_successful
       expect(response).to have_http_status(200)
     end
   end
--- spec/controllers/v1/services_controller_spec.rb
+++ spec/controllers/v1/services_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe V1::ServicesController, type: :controller do
   describe 'GET #show' do
     it 'responds successfully with an HTTP 200 and service url' do
       get :show
-      expect(response).to be_success
+      expect(response).to be_successful
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq('application/json')
       json = JSON.parse(response.body)
