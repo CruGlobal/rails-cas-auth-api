1ac50e04caa59a640eae39e3b3ada6c607965782
Justin Sabelko <justin.sabelko@cru.org>
2020-08-31 13:53:49 -0500

    rails app:update and changes from railsdiff.org

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "monitors/lb"
   api_version(module: "V1", path: {value: "v1"}) do
